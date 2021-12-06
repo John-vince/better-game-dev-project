e90570125b9c7b60a0099530d439ffe36ca86126
John-vince <johnvince53@gmail.com>
2021-12-06 10:00:46 +0200

    more work on the character class

--- Form1.cs
+++ Form1.cs
@@ -25,29 +25,30 @@ namespace better_game_dev_project
 
     class tile //Tile is the base class for all in‐game objects that have positions on the map.
     {
+        // our vars for tile
         int x, y;
-       public enum TileType
+       public enum TileType // our enums to dertermin the tile type.
         {
             Hero,
             Enemy,
             Gold,
             Weapon
         }
-        public int Get_x()
+        public int Get_x() // gives caller the x value. I donno what else to tell yea cheef
         {
             return x;
         }
-        public int Get_y()
+        public int Get_y() // gives caller the y value. I donno what else to tell yea cheef
         {
             return y;
         }
 
-        public void tile_Constructor()
+        public void tile_Constructor() // just an overload constructor
         {
 
         }
 
-        public void tile_Constructor(int _x, int _y)
+        public void tile_Constructor(int _x, int _y) // the true constructor that brings this object into being.
         { 
             x = _x; 
             y = _y; 
@@ -56,15 +57,48 @@ namespace better_game_dev_project
 
     }
 
-    class Obstacle : tile
+    class Obstacle : tile // just provides obstacles.
     {
-        public void Constructor(int _x, int _y)
+        public void Obstacle_Constructor(int _x, int _y)
         {
             this.tile_Constructor(_x, _y);
         }
     }
+
+    class emptytile : tile
+    {
+        public void emptytile_constructor(int _x,int _y) // just adds an empty space
+        {
+            this.tile_Constructor(_x,_y);
+        }
+    }
     class Character : tile //true base class for Hero and Goblin classes.
     {
+        int HP, Max_hp, Damage;
+        public int Get_HP()
+        { return HP; }
+        public int Get_Max_Hp()
+            { return Max_hp; }
+        public int Get_Damage()
+        {
+            return Damage;
+        }
+        string[] Tile;
+
+        public enum Movement
+        {
+            No_movement
+            , Up
+            , Down
+            , Left
+            , Right
+        }
+
+        public void Constructor_Character(int x, int y)
+        {
+            this.tile_Constructor(x, y);
+        }
+
 
     }
     class Enemy : Character //contains a single protected member variable that is used internally by subclasses
